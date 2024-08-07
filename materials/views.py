01bd7a94b69ca9a79e032e1efcb38afb24963b57
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-08-07 12:06:57 +0300

    Добавлена проверка на владельца и модератора в контролеры Курсов Урок… …ов И Тестов. Permission на просмотр остался свободным для авторизованных пользователей.

--- materials/views.py
+++ materials/views.py
@@ -8,11 +8,21 @@ from materials.serializers import (
     TestSerializer,
     AttemptAnswerSerializer,
 )
+from users.permissions import IsOwner, IsModerator
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        if self.action in "create":
+            self.permission_classes = (IsModerator,)
+        elif self.action in "update":
+            self.permission_classes = (IsModerator | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (IsModerator | IsOwner,)
+            return super().get_permissions()
 
 
 class LessonViewSet(ModelViewSet):
@@ -24,6 +34,15 @@ class LessonViewSet(ModelViewSet):
         lesson.owner = self.request.user
         lesson.save()
 
+    def get_permissions(self):
+        if self.action in "create":
+            self.permission_classes = (IsModerator,)
+        elif self.action in "update":
+            self.permission_classes = (IsModerator | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (IsModerator | IsOwner,)
+        return super().get_permissions()
+
 
 class TestViewSet(ModelViewSet):
     queryset = Test.objects.all()
@@ -33,6 +52,15 @@ class TestViewSet(ModelViewSet):
         test = serializer.save()
         test.owner = self.request.user
         test.save()
+
+    def get_permissions(self):
+        if self.action in "create":
+            self.permission_classes = (IsModerator,)
+        elif self.action in "update":
+            self.permission_classes = (IsModerator | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (IsModerator | IsOwner,)
+        return super().get_permissions()
 
 
 class AttemptAnswerCreateAPIView(generics.CreateAPIView):
---
+++ users/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+
+class IsModerator(permissions.BasePermission):
+    """Проверяем, является ли пользователь модератором"""
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="moderator").exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """Проверяем, является ли пользователь владельцем"""
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
+
